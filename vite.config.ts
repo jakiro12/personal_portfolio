b8e0f5502631143ab0c6ea0a0c9e0d4d775b64c1
Jakiro12 <eclipsisnocte@gmail.com>
2025-08-06 19:15:14 -0300

    test removed

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test:{
-    globals:true,
-    environment:'jsdom',
-    setupFiles:'./src/setupTests.ts',
-    css:true
-  },  
+  plugins: [react()], 
 }satisfies UserConfig)
